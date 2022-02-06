cf0705e2531b930df60a5ed14621ef1b1bfba8e4
primenumber <prime@kmc.gr.jp>
2022-02-06 17:39:24 +0900

    Implement LW instruction

--- src/main/scala/ituki/Core.scala
+++ src/main/scala/ituki/Core.scala
@@ -3,11 +3,12 @@ package ituki
 import chisel3._
 import chisel3.util._
 import common.Consts._
+import common.Instructions._
 
 class Core extends Module {
   val io = IO(new Bundle {
     val imem = Flipped(new ImemPortIo())
-
+    val dmem = Flipped(new DmemPortIo())
     val exit = Output(Bool())
   })
 
@@ -21,23 +22,42 @@ class Core extends Module {
   io.imem.addr := pc_reg
   val inst = io.imem.inst
 
-  // Decode stage
+  // Instruction Decode (ID) stage
   val rs1_addr = inst(19, 15)
   val rs2_addr = inst(24, 20)
   val wb_addr = inst(11, 7)
+  val imm_i = inst(31, 20)
+  val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i) // sign extension
 
   val rs1_data = Mux((rs1_addr =/= 0.U(REG_ADDR_BITS.W)), regfile(rs1_addr), 0.U(WORD_LEN.W))
   val rs2_data = Mux((rs2_addr =/= 0.U(REG_ADDR_BITS.W)), regfile(rs2_addr), 0.U(WORD_LEN.W))
 
-  // exit
-  io.exit := (inst === 0x34333231.U(WORD_LEN.W))
+  // Execution (EX) stage
 
-  printf(p"pc_reg   : 0x${Hexadecimal(pc_reg)}\n")
-  printf(p"inst     : 0x${Hexadecimal(inst)}\n")
-  printf(p"rs1_addr : ${rs1_addr}\n")
-  printf(p"rs2_addr : ${rs2_addr}\n")
-  printf(p"wb_addr  : ${wb_addr}\n")
-  printf(p"rs1_data : 0x${Hexadecimal(rs1_data)}\n")
-  printf(p"rs2_data : 0x${Hexadecimal(rs2_data)}\n")
+  val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
+    (inst === LW) -> (rs1_data + imm_i_sext)
+  ))
+
+  // Memory Access (MEM) stage
+
+  io.dmem.addr := alu_out
+
+  val wb_data = io.dmem.rdata
+  when (inst === LW) {
+    regfile(wb_addr) := wb_data
+  }
+
+  // exit
+  io.exit := (inst === 0x24232221.U(WORD_LEN.W))
+
+  printf(p"pc_reg     : 0x${Hexadecimal(pc_reg)}\n")
+  printf(p"inst       : 0x${Hexadecimal(inst)}\n")
+  printf(p"rs1_addr   : ${rs1_addr}\n")
+  printf(p"rs2_addr   : ${rs2_addr}\n")
+  printf(p"wb_addr    : ${wb_addr}\n")
+  printf(p"rs1_data   : 0x${Hexadecimal(rs1_data)}\n")
+  printf(p"rs2_data   : 0x${Hexadecimal(rs2_data)}\n")
+  printf(p"wb_data    : 0x${Hexadecimal(wb_data)}\n")
+  printf(p"dmem.addr  : ${io.dmem.addr}\n")
   printf("--------\n")
 }
