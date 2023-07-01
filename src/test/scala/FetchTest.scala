e051d1b52c7dfcf354f76799a6846cf60af987d1
primenumber <prime@kmc.gr.jp>
2023-07-01 20:59:46 +0900

    Fix test

--- src/test/scala/FetchTest.scala
+++ src/test/scala/FetchTest.scala
@@ -2,11 +2,11 @@ package ituki
 
 import chisel3._
 
-import org.scalatest._
+import org.scalatest.freespec.AnyFreeSpec
 import chiseltest._
 
-class HexTest extends FlatSpec with ChiselScalatestTester {
-  "ituki" should "work through hex" in {
+class HexTest extends AnyFreeSpec with ChiselScalatestTester {
+  "ituki should work through hex" in {
     test(new Top) { c =>
       while (!c.io.exit.peek().litToBoolean) {
         c.clock.step(1)
