b07e544f7afe34d98f7c6f17e6cbc993cb24ce3d
primenumber <prime@kmc.gr.jp>
2023-07-05 02:41:12 +0900

    Run multiple hex

--- src/test/scala/RiscvTests.scala
+++ src/test/scala/RiscvTests.scala
@@ -6,13 +6,49 @@ import org.scalatest.flatspec.AnyFlatSpec
 import chiseltest._
 
 class RiscvTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "ituki"
+  val testData: List[String] = List[String](
+    "test/hex/rv32mi-p-csr.hex",
+    "test/hex/rv32ui-p-add.hex",
+    "test/hex/rv32ui-p-addi.hex",
+    "test/hex/rv32ui-p-and.hex",
+    "test/hex/rv32ui-p-andi.hex",
+    "test/hex/rv32ui-p-auipc.hex",
+    "test/hex/rv32ui-p-beq.hex",
+    "test/hex/rv32ui-p-bge.hex",
+    "test/hex/rv32ui-p-bgeu.hex",
+    "test/hex/rv32ui-p-blt.hex",
+    "test/hex/rv32ui-p-bltu.hex",
+    "test/hex/rv32ui-p-bne.hex",
+    "test/hex/rv32ui-p-jal.hex",
+    "test/hex/rv32ui-p-jalr.hex",
+    "test/hex/rv32ui-p-lui.hex",
+    "test/hex/rv32ui-p-lw.hex",
+    "test/hex/rv32ui-p-or.hex",
+    "test/hex/rv32ui-p-ori.hex",
+    "test/hex/rv32ui-p-sll.hex",
+    "test/hex/rv32ui-p-slli.hex",
+    "test/hex/rv32ui-p-slt.hex",
+    "test/hex/rv32ui-p-slti.hex",
+    "test/hex/rv32ui-p-sltiu.hex",
+    "test/hex/rv32ui-p-sltu.hex",
+    "test/hex/rv32ui-p-sra.hex",
+    "test/hex/rv32ui-p-srai.hex",
+    "test/hex/rv32ui-p-srl.hex",
+    "test/hex/rv32ui-p-srli.hex",
+    "test/hex/rv32ui-p-sub.hex",
+    "test/hex/rv32ui-p-sw.hex",
+    "test/hex/rv32ui-p-xor.hex",
+    "test/hex/rv32ui-p-xori.hex",
+  )
   it should "work through hex" in {
-    test(new Top("test/hex/rv32ui-p-add.hex")) { c =>
-      while (!c.io.exit.peek().litToBoolean) {
-        c.clock.step(1)
+    testData.foreach { f =>
+      test(new Top(f)) { c =>
+        while (!c.io.exit.peek().litToBoolean) {
+          c.clock.step(1)
+        }
+        c.io.gp.expect(1.U)
       }
-      c.io.gp.expect(1.U)
     }
   }
-  
 }
