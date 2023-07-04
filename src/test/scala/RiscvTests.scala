5ae1016873ad7b112e2710e43e07a8852175b3c5
primenumber <prime@kmc.gr.jp>
2023-07-05 02:03:28 +0900

    Fix

--- src/test/scala/RiscvTests.scala
+++ src/test/scala/RiscvTests.scala
@@ -2,11 +2,11 @@ package ituki
 
 import chisel3._
 
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import chiseltest._
 
-class RiscvTest extends AnyFreeSpec with ChiselScalatestTester {
-  "ituki should work through hex" in {
+class RiscvTest extends AnyFlatSpec with ChiselScalatestTester {
+  it should "work through hex" in {
     test(new Top("test/hex/rv32ui-p-add.hex")) { c =>
       while (!c.io.exit.peek().litToBoolean) {
         c.clock.step(1)
